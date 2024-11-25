3e9490c1dca717ae0f1e7e664a16b31c1c64c279
bbuuiipp <88738048+bbuuiipp@users.noreply.github.com>
2024-11-25 00:54:05 -0500

    added user input

--- pages/3_Weather.py
+++ pages/3_Weather.py
@@ -2,9 +2,25 @@ import streamlit as st
 import pandas as pd
 import requests as rq
 import CityTranslate as translate
+import re
 
 st.header("Weather Forecast!")
 
-response = rq.get("https://api.weather.gov/openapi.json")
 
-st.write(translate.fetch_weather("565 Elgaen Ct", "Roswell", "GA", "30075"))
+def parseAddress(address):
+    #captures the individual parts of the string
+    correctPattern = r'^(.*?),\s*(.*?),\s*([A-Z]{2})\s*(\d{5})(?:-\d{4})?$'
+    #uses re module to match the addresss input to the pattern given
+    match = re.match(correctPattern, address)
+    if match:
+        street, city, state, zipCode = match.groups()
+        return {
+            "street": street,
+            "city": city,
+            "state": state,
+            "zip": zipCode
+        }
+
+address = parseAddress(st.text_input("Enter your full address ('123 Main St, City, ST 12345'): "))
+
+st.write(translate.fetch_weather(address["street"], address["city"], address["state"], address["zip"]))
